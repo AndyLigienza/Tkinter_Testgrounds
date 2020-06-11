5181388d5e5b6917100c672791204c9cc7e0856c
Andy Ligienza <andrzej.ligienza@gmail.com>
2020-06-12 00:03:32 +0200

    Experimenting with buttons

--- Project1/app.py
+++ Project1/app.py
@@ -1,7 +1,14 @@
 import tkinter as tk
 from tkinter import ttk #set of themable widgets
 
-root = tk.Tk()
-ttk.Label(root, text="Hello, World!", padding = (30, 10)).pack()
+def greet():
+    print("Hello, World!")
 
+root = tk.Tk()
+
+greet_button = ttk.Button(root, text="Greet", command=greet)
+greet_button.pack(side="left", fill="both", expand=True) # algoritm Pack that embeds the widget
+
+quit_button = ttk.Button(root, text="Quit", command=root.destroy)
+quit_button.pack(side="left")
 root.mainloop()
