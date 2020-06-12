1b99bb2ed83be06d1612cd4799e73900e3097eaf
Andy Ligienza <andrzej.ligienza@gmail.com>
2020-06-12 14:53:24 +0200

    Added entry field

--- Project1/app.py
+++ Project1/app.py
@@ -2,13 +2,23 @@ import tkinter as tk
 from tkinter import ttk #set of themable widgets
 
 def greet():
-    print("Hello, World!")
+    print(f"Hello, {user_name.get() or 'World'}!")
 
-root = tk.Tk()
+root = tk.Tk() # main window declaration
+
+user_name = tk.StringVar()
+
+name_label = ttk.Label(root, text="Name: ")
+name_label.pack(side="left", padx=(0, 10))
+
+name_entry = ttk.Entry(root, width=15, textvariable=user_name)
+name_entry.pack(side="left")
+name_entry.focus()
 
 greet_button = ttk.Button(root, text="Greet", command=greet)
-greet_button.pack(side="left", fill="both", expand=True) # algoritm Pack that embeds the widget
+greet_button.pack(side="left", fill="x", expand=True) # algoritm Pack that embeds the widget
 
 quit_button = ttk.Button(root, text="Quit", command=root.destroy)
-quit_button.pack(side="left")
-root.mainloop()
+quit_button.pack(side="left", fill="x", expand=True) # fill value can be x,y or both
+
+root.mainloop() #main window initialization
