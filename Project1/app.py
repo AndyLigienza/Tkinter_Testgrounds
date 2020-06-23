5ec4311055dc7d32c0b7c6ec022cb1efdfaaf4e2
Andy Ligienza <andrzej.ligienza@gmail.com>
2020-06-23 21:36:18 +0200

    Expanded greeter with frames

--- Project1/app.py
+++ Project1/app.py
@@ -5,20 +5,27 @@ def greet():
     print(f"Hello, {user_name.get() or 'World'}!")
 
 root = tk.Tk() # main window declaration
+root.title("Greeter")
 
 user_name = tk.StringVar()
 
-name_label = ttk.Label(root, text="Name: ")
+input_frame = ttk.Frame(root, padding=(20,10,20,0)) #padding (left, top, right, bottom)
+input_frame.pack(fill="both")
+
+name_label = ttk.Label(input_frame, text="Name: ")
 name_label.pack(side="left", padx=(0, 10))
 
-name_entry = ttk.Entry(root, width=15, textvariable=user_name)
+name_entry = ttk.Entry(input_frame, width=15, textvariable=user_name)
 name_entry.pack(side="left")
 name_entry.focus()
 
-greet_button = ttk.Button(root, text="Greet", command=greet)
+buttons = ttk.Frame(root, padding=(20,10)) # padding (lef/right, top/bottom)
+buttons.pack(fill="both")
+
+greet_button = ttk.Button(buttons, text="Greet", command=greet)
 greet_button.pack(side="left", fill="x", expand=True) # algoritm Pack that embeds the widget
 
-quit_button = ttk.Button(root, text="Quit", command=root.destroy)
+quit_button = ttk.Button(buttons, text="Quit", command=root.destroy)
 quit_button.pack(side="left", fill="x", expand=True) # fill value can be x,y or both
 
 root.mainloop() #main window initialization
